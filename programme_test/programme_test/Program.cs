42042f99119828255b42d02b01ef74669de14350
zagnire <zagnireshadowmen@gmail.com>
2018-03-09 17:06:41 +0100

    commencement

---
+++ programme_test/programme_test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace programme_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=0 ;
+            n = n + 1;
+        
+        }
+    }
+}
